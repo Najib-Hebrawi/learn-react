7cce9a74fc3bc6080d68c5a18aa4220ce4686787
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-02-27 12:37:15 +0100

    css external style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,9 @@ import React from "react";
 
 
 function Header(){
-   
-        const style =
-            { 
-                color: 'red',
-                fontSize :'30px',
-                backgroundColor:'black'
-            }
-        
 
         return(
-        <h1 style={style}>Design is my life</h1>
+        <h1>Design is my life</h1>
 
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,9 @@ import React from "react";
 import  ReactDOM, {render}  from "react-dom";
 import App from './components/App.js'
 
+import './style.css'
 
 
 
 
-ReactDOM.render(
-/*
-// this is called JSX, DVS is a new way to write Javascript.
-    <div style={{color: 'red'}}>
-        <h1>Test</h1>
-        <p>Here is Test</p>
-        <ul>
-            <li>Facebook</li>
-            <li>youtube</li>
-            <li>instagram</li>
-        </ul>
-    </div>,     
-*/
-<App/>
-,document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
