d70c577169b58ca4deb59848c441179ce41dbeab
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-02-27 10:48:29 +0100

    App.js is ready

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function App (){
+    return(
+
+        <div>
+        <h1>Test</h1>
+        <p>Here is Test</p>
+        <ul>
+            <li>Facebook</li>
+            <li>youtube</li>
+            <li>instagram</li>
+        </ul>
+    </div>
+
+    );
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import  ReactDOM, {render}  from "react-dom";
+import App from './App.js';
+
+
+
+
 
 ReactDOM.render(
+/*
 // this is called JSX, DVS is a new way to write Javascript.
-    <div>
+    <div style={{color: 'red'}}>
         <h1>Test</h1>
         <p>Here is Test</p>
         <ul>
@@ -12,7 +18,6 @@ ReactDOM.render(
             <li>instagram</li>
         </ul>
     </div>,     
-document.getElementById('root'));
-
-
-
+*/
+<App/>
+,document.getElementById('root'));
