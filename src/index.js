4b7001c9e0de9b400e57f60dd1e282d77eef2fef
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-02-27 10:20:50 +0100

    impot render method

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  ReactDOM, {render}  from "react-dom";
+
+ReactDOM.render(
+<div>
+<h1>Test</h1>
+<p>Here is Test</p>
+</div>,     
+document.getElementById('root'));
+
+
+
