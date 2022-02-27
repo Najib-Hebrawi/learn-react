133316d39ac8619621e4d9db31cdd5a63cdfbca5
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-02-27 10:24:30 +0100

    how to write JSX

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import  ReactDOM, {render}  from "react-dom";
 
 ReactDOM.render(
-<div>
-<h1>Test</h1>
-<p>Here is Test</p>
-</div>,     
+// this is called JSX, DVS is a new way to write Javascript.
+    <div>
+        <h1>Test</h1>
+        <p>Here is Test</p>
+        <ul>
+            <li>Facebook</li>
+            <li>youtube</li>
+            <li>instagram</li>
+        </ul>
+    </div>,     
 document.getElementById('root'));
 
 
