b355eaf99ef6933fe719233219663a64ee311ac5
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-16 12:48:59 +0100

    some changes

---
+++ src/components_state/App.js
@@ -0,0 +1,27 @@
+import React,{Component} from "react";
+
+
+
+
+class App extends Component{
+
+    constructor(){
+        super()
+        this.state={answer:"yes"}
+         
+    }
+
+
+
+    render(){
+        return(
+            <h1>Do you love me? {this.state.answer}</h1>
+        )
+    }
+
+
+
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import  ReactDOM from "react-dom";
-import App from "./components_class/App";
+import App from "./components_state/App";
 import './style.css'
 
 
